3cd38105bac96c7e0106b195ed49e2e081fa86e6
unknown <souravyadavcs20@pgdav.du.ac.in>
2025-08-03 11:33:35 +0530

    edited homepage

--- application/controllers/main_view.py
+++ application/controllers/main_view.py
@@ -2,9 +2,11 @@ from flask import render_template, request, redirect,make_response, send_file
 from flask import Flask, session
 from datetime import datetime
 from flask import current_app as app
+from datetime import datetime
 
 @app.route('/')
 def home_page():
     if request.method=="GET":
-        return render_template("home.html")
+        current_datetime = datetime.now()
+        return render_template("home.html",date=current_datetime)
     
